3d1fd86719117f417f22d6cb16e14281149f8de5
Ciahaka <wilddiver.by@gmail.com>
2022-05-28 22:13:01 +0300

    commit_8

--- src/Post/Post.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Post = () => {
-    return (
-        <div>
-            <h2>Post23</h2>
-        </div>
-    )
-}
--- src/Test/Test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Test = () => {
-    return (
-        <div>
-            <h2>Test111</h2>
-        </div>
-    )
-}
