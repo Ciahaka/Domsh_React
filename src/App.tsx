ea1eb9913d9391505f8c1632b49d0673efb6f4f0
Ciahaka <wilddiver.by@gmail.com>
2022-05-29 22:53:39 +0300

    App_forNewTask_02

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import './App.css';
 import {NewComponent} from "./NewComponent";
+import {TopCars} from "./TopCars";
 
 function App() {
-    const [students, setStudents] = useState([
+
+    /*const [students, setStudents] = useState([
         {id: 1, name: "James", age: 8},
         {id: 2, name: "Robert", age: 18},
         {id: 3, name: "John", age: 28},
@@ -15,10 +17,21 @@ function App() {
         {id: 9, name: "Thomas", age: 88},
         {id: 10, name: "Charles", age: 98},
         {id: 11, name: "Christopher", age: 100},
-    ]);
+]);
 
     return (
         <NewComponent students={students}/>
+    );*/
+
+    const topCars = [
+        {id:1,manufacturer:'BMW', model:'m5cs'},
+        {id:2,manufacturer:'Mercedes', model:'e63s'},
+        {id:3,manufacturer:'Audi', model:'rs6'}
+    ]
+
+
+    return (
+        <TopCars  topCars={topCars}/>
     )
 }
 export default App;
