1e722d358ddf82ff91bb5a97dd68166f1db6ac16
Ciahaka <wilddiver.by@gmail.com>
2022-05-30 00:17:06 +0300

    Edits_forNewTask_02

--- src/TopCars.tsx
+++ src/TopCars.tsx
@@ -8,30 +8,28 @@ type topCars = {
     manufacturer: string,
     model: string
 }
-
 export const TopCars = (props: TopCarsType) => {
-    debugger
     return (
-
         <table>
+            <caption>Top cars</caption>
+            <thead>
             <tr>
                 <th>id</th>
                 <th>manufacturer</th>
                 <th>model</th>
             </tr>
-            <caption>Top cars</caption>
-            {props.topCars.map((objectFromTopCarsArray, index,) => {
+            </thead>
+            {props.topCars.map((objectFromTopCarsArray, index) => {
                 return (
-                    <>
-                        <tr key={index + 1}>
-                            <td>{objectFromTopCarsArray.id}</td>
-                            <td>{objectFromTopCarsArray.manufacturer}</td>
-                            <td>{objectFromTopCarsArray.model}</td>
-                        </tr>
-                    </>
+                    <tbody key={index+1}>
+                    <tr>
+                        <td>{objectFromTopCarsArray.id}</td>
+                        <td>{objectFromTopCarsArray.manufacturer}</td>
+                        <td>{objectFromTopCarsArray.model}</td>
+                    </tr>
+                    </tbody>
                 )
             })}
         </table>
     )
-        ;
 }
